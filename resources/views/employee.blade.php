df0b3703f1029ab172907af786788acc37274bd2
aisyahnabila <aisyahnabila2.an@gmail.com>
2024-09-28 15:38:03 +0700

    employee page

--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -1,7 +1,44 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    ini employee
-</div>
+    <!-- Hero Section Start -->
+    <section class="hero-section text-center">
+        <div class="hero-content">
+            <h1 class="hero-title">PEGAWAI KAMI</h1>
+        </div>
+    </section>
+    <!-- Hero Section End-->
 
+    <div class="container">
+        <!-- Content Section -->
+        <div class="container mt-5">
+            <div class="row text-center">
+                <div class="col-lg-12">
+                    <h2 class="text-primary">KETUA COE BHT</h2>
+                    <img src="{{ asset('../assets\img\foto_formal1.png') }}" class="img-fluid mb-3" alt="Ketua COE BHT">
+                    <p>SUTO DERMAWAN M.T</p>
+                </div>
+            </div>
+
+            <!-- Other Staff Start -->
+            <div class="row text-center mt-5">
+                <h2 class="text-primary mb-4">PEGAWAI COE BHT</h2>
+                <div class="col-md-4">
+                    <img src="{{ asset('../assets\img\foto_formal1.png') }}" class="img-fluid mb-2" alt="Staff">
+                    <p>SUTO DERMAWAN M.T</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="{{ asset('../assets\img\foto_formal1.png') }}" class="img-fluid mb-2" alt="Staff">
+                    <p>SUTO DERMAWAN M.T</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="{{ asset('../assets\img\foto_formal1.png') }}" class="img-fluid mb-2" alt="Staff">
+                    <p>SUTO DERMAWAN M.T</p>
+                </div>
+            </div>
+            <!-- Other Staff End -->
+
+        </div>
+        <!-- Content Section End-->
+
+    </div>
 @endsection
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top">
     <div class="container">
         <a class="navbar-brand" href="#">
-            <img src="{{asset('assets/img/logo.png')}}" alt="Logo">
+            <img src="{{ asset('assets/img/logo.png') }}" alt="Logo">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,7 +10,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" href="{{route('index')}}">Home</a>
+                    <a class="nav-link active" href="{{ route('index') }}">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Pelatihan & Seminar</a>
@@ -24,11 +24,11 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Pengmas</a>
                 </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('staff') }}">Staf</a>
+                    </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Tentang</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('staff')}}">Staf</a>
                 </li>
             </ul>
         </div>
