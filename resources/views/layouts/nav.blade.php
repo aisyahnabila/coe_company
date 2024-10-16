3f5739a91f60320659feb962b8c60256a2bf2a71
Dennydaffa <dennydaffarizaldy16@gmail.com>
2024-10-16 18:41:03 +0700

    Bug Update

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -3,41 +3,53 @@
         <a class="navbar-brand" href="#">
             <img src="{{ asset('assets/img/logo.png') }}" alt="Logo">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler navbar-hamburger" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('index') ? 'active' : '' }}"
-                        href="{{ route('index') }}">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('training') ? 'active' : '' }}"
-                        href="{{ url('/') }}#training">Pelatihan & Seminar</a>
-                </li>
 
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('penelitian') ? 'active' : '' }}"
-                        href="#">Penelitian</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('kerjasama') ? 'active' : '' }}"
-                        href="#">Kerjasama</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('pengmas') ? 'active' : '' }}" href="#">Pengmas</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('staff') ? 'active' : '' }}"
-                        href="{{ route('staff') }}">Staf</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('tentang') ? 'active' : '' }}" href="#">Tentang</a>
-                </li>
-            </ul>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link {{ request()->routeIs('ss') ? 'active' : '' }}"
+                            href="{{ route('index') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ request()->routeIs('tentang') ? 'active' : '' }}"
+                            href="#tentang">Tentang</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ request()->routeIs('seminar') ? 'active' : '' }}"
+                            href="{{ url('/') }}#seminar">Pelatihan & Seminar</a>
+                    </li>
+                    {{-- <li class="nav-item">
+                        <a class="nav-link {{ request()->routeIs('penelitian') ? 'active' : '' }}" href="#">Penelitian</a>
+                    </li> --}}
+                    <li class="nav-item">
+                        <a class="nav-link {{ request()->routeIs('kerjasama') ? 'active' : '' }}"
+                            href="{{ url('/') }}#kerjasama">Kerja sama</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ request()->routeIs('pengmas') ? 'active' : '' }}"
+                            href="{{ url('/') }}#pengabdian">Pengmas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ request()->routeIs('mitra') ? 'active' : '' }}"
+                            href="{{ url('/') }}#mitra">Mitra</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ request()->routeIs('ss') ? 'active' : '' }}"
+                            href="{{ route('staff') }}">Staf</a>
+                    </li>
 
+                </ul>
+            </div>
         </div>
     </div>
 </nav>
+
